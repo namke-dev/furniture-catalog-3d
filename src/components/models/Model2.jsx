ee617c11f63dd82bc3f46bde1019258e4c526ee4
namke-dev <namke.dev@gmail.com>
2024-01-23 23:26:03 +0700

    format code

--- src/components/models/Model2.jsx
+++ src/components/models/Model2.jsx
@@ -4,13 +4,13 @@ import { useFrame } from "@react-three/fiber";
 
 export function Model2(props) {
   const { nodes, materials } = useGLTF("/models/model2/scene.gltf");
-  const mesh = useRef(null);
+  const modelRef = useRef(null);
   useFrame((state, delta) => {
-    mesh.current.rotation.y += delta * 0.1;
+    modelRef.current.rotation.y += delta * 0.1;
   });
 
   return (
-    <group {...props} dispose={null} ref={mesh}>
+    <group {...props} dispose={null} ref={modelRef} scale={2}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
         <mesh geometry={nodes.Object_18.geometry} material={materials.VIDRO} />
         <mesh geometry={nodes.Object_15.geometry} material={materials.BASE} />
